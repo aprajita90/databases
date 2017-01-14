0b5ae08ec960069167f470f2e61f06a8f7775677
aprajita90 <aprajitajain90@gmail.com>
2017-01-14 15:27:04 -0800

    Clean up comments from models index.js

--- server/models/index.js
+++ server/models/index.js
@@ -3,25 +3,15 @@ var db = require('../db');
 module.exports = {
   messages: {
     get: function () {
-      
-      // var post = {id: null, username: 'John', message: 'Hello World!', roomname: 'SFRoom'};
-      // connection.query('INSERT INTO messages SET ?', post, function(err, result) {
-      //   console.log('Successful data inpus');
-      // });
     }, // a function which produces all the messages
     post: function () {
-      console.log('Successful data inpus POST FROM INDEX OF MODELS');
-      // var post = {id: null, username: 'John', message: 'Hello World!', roomname: 'SFRoom'};
-      // connection.query('INSERT INTO messages SET ?', post, function(err, result) {
-      //   console.log('Successful data inpus');
-      // });
     } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
-    get: function () {console.log('Successful data inpus GET FROM INDEX OF MODELS');},
-    post: function () {console.log('Successful data inpus GET FROM INDEX OF MODELS');}
+    get: function () {},
+    post: function () {} 
   }
 };
 
